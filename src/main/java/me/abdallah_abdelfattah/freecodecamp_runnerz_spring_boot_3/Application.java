4f7da2f20508d73db6a25ca1729a05bdd5dd6f7d
AbdAllah <abdallah.a.elfatah@gmail.com>
2024-10-17 09:39:43 +0300

    init commit

---
+++ src/main/java/me/abdallah_abdelfattah/freecodecamp_runnerz_spring_boot_3/Application.java
@@ -0,0 +1,13 @@
+package me.abdallah_abdelfattah.freecodecamp_runnerz_spring_boot_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/me/abdallah_abdelfattah/freecodecamp_runnerz_spring_boot_3/RunnerzApplicationTests.java
@@ -0,0 +1,13 @@
+package me.abdallah_abdelfattah.freecodecamp_runnerz_spring_boot_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunnerzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
