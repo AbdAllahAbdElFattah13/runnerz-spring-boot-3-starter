4900e1a46d01f48ed5db327287a135bf0717d718
AbdAllah <abdallah.a.elfatah@gmail.com>
2024-10-17 21:01:36 +0300

    add findById

--- src/main/java/me/abdallah_abdelfattah/freecodecamp_runnerz_spring_boot_3/run/RunController.java
+++ src/main/java/me/abdallah_abdelfattah/freecodecamp_runnerz_spring_boot_3/run/RunController.java
@@ -1,6 +1,7 @@
 package me.abdallah_abdelfattah.freecodecamp_runnerz_spring_boot_3.run;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +20,10 @@ public class RunController {
     @GetMapping("")
     List<Run> findAll() {
         return runRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    Run findById(@PathVariable String id) {
+        return runRepository.findById(id);
     }
 }
